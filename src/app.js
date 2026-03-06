90aa59082cd72be74d32efadac9e5af920463b3a
Connor <connorjohnhughes@gmail.com>
2026-03-05 19:58:17 -0800

    lab complete

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
 import defaultRouter from './routers/routes.js';
+import session from 'express-session';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const SECRET = process.env.SESSION_SECRET;
 
 //configure Express.js app
 const app = express();
+
+
 
 //view engine
 app.set("view engine", "ejs");
@@ -14,6 +23,26 @@ app.use(express.static('public'));
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//session first
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true
+}))
+
+// attach user to every request second
+app.use((req, res, next) => {
+    if(req.session.user) {
+        req.user = req.session.user;
+    }else{
+        req.user = null;
+    };
+    next();
+})
+
+
+
 
 //routers
 app.use("/", defaultRouter);
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
-import { createUser, findUserByUsername } from "../services/user.service.js";
+import { createUser, findUserByUsername, validatePassword } from "../services/user.service.js";
 
-const loginPage = (req, res) => {
+ const loginPage = (req, res) => {
     res.render("login", {
         title: "Login",
         errors: req.query.errors || null
@@ -17,24 +17,77 @@ const registerPage = (req, res) => {
 const register = async (req, res) => {
     const { username, password, confirm, role } = req.body;
 
-    if (!username || !password || password !== confirm) {
-        return res.redirect("/register?errors=Invalid registration details");
-    }
+    if (!username || !password || !confirm) {
+        return res.redirect("/register?errors=All fields required");
+    };
+
+    if(password !== confirm){
+        return res.redirect("/register?errors=Password do not match!!");
+    };
+
+    if (role !== "user" && role !== "admin"){
+        return res.redirect("/register?errors=Invalid role!!!");
+    };
 
     await createUser(username, password, role);
-    res.redirect("/login");
+    return res.redirect("/login");
 };
 
 const login = async (req, res) => {
     const { username, password } = req.body;
 
+    if(!username || !password ) {
+        return res.redirect("/login?errors=All fields required");
+    };
+
     const user = await findUserByUsername(username);
 
-    if (!user || user.password !== password) {
+    if (!user) {
         return res.redirect("/login?errors=Invalid credentials");
     }
 
-    res.redirect("/dashboard");
+    const isValid = await validatePassword(password, user.password);
+
+    if(!isValid){
+        return res.redirect("/login?errors=Invalid credentials!!")
+    }
+
+    req.session.user = {
+        userID: user.userID,
+        username: user.username,
+        role: user.role
+    }
+
+    return res.redirect("/dashboard");
 };
+
+
+// eslint-disable-next-line consistent-return
+export const isLoggedIn = (req, res , next) => {
+    if(!req.user) {
+        return res.redirect("/login?errors=Please log in first");
+    };
+    next();
+};
+
+
+
+// eslint-disable-next-line arrow-body-style
+export const hasRole = (role) => {
+
+    // eslint-disable-next-line consistent-return
+    return(req, res, next) => {
+        if (!req.user || req.user.role !== role){
+            return res.redirect("/login?errors=Access denied")
+        };
+        next();
+    }
+}
+
+export const logout = (req, res) => {
+    req.session.destroy(() => {
+        return res.redirect("/login");
+    })
+}
 
 export default { loginPage, registerPage, register, login };
